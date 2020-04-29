79648426ad3f98ee2bb26c116942b9d1ff189d6a
maxild <mmaxild@gmail.com>
2020-04-29 19:01:49 +0200

    Loading tool versions from build.config

--- build.ps1
+++ build.ps1
@@ -1,8 +1,4 @@
-##########################################################################
-# This is the Cake bootstrapper script for PowerShell.
-# This file was downloaded from https://github.com/cake-build/resources
-# Feel free to change this file to fit your needs.
-##########################################################################
+#!/usr/bin/env pwsh
 
 <#
 
@@ -19,8 +15,6 @@ The task/target to run.
 The build configuration to use.
 .PARAMETER Verbosity
 Specifies the amount of information to be displayed.
-.PARAMETER ShowVersion
-Show version of Cake tool.
 .PARAMETER ScriptArgs
 Remaining arguments are added here.
 
@@ -36,30 +30,44 @@ Param(
     [string]$Configuration = "Release",
     [ValidateSet("Quiet", "Minimal", "Normal", "Verbose", "Diagnostic")]
     [string]$Verbosity = "Verbose",
-    [switch]$ShowVersion,
-    [Alias("DryRun", "Noop")]
     [Parameter(Position = 0, Mandatory = $false, ValueFromRemainingArguments = $true)]
     [string[]]$ScriptArgs
 )
 
 $PSScriptRoot = split-path -parent $MyInvocation.MyCommand.Definition;
 
-# Tree
-$TOOLS_DIR = Join-Path $PSScriptRoot "tools"
+###########################################################################
+# LOAD versions from build.config
+###########################################################################
 
-# TODO: Maybe refactor into build.config
-$CakeVersion = "0.37.0"
-$GitVersionVersion = "5.0.1"
-
-# Make sure tools folder exists
-if ((Test-Path $PSScriptRoot) -and (-not (Test-Path $TOOLS_DIR))) {
-    Write-Verbose -Message "Creating tools directory..."
-    New-Item -Path $TOOLS_DIR -Type directory | out-null
+[string] $CakeVersion = ''
+[string] $GitVersionVersion = ''
+foreach ($line in Get-Content (Join-Path $PSScriptRoot 'build.config')) {
+    if ($line -like 'CAKE_VERSION=*') {
+        $CakeVersion = $line.SubString(13)
+    }
+    elseif ($line -like 'GITVERSION_VERSION=*') {
+        $GitVersionVersion = $line.SubString(19)
+    }
+}
+if ([string]::IsNullOrEmpty($CakeVersion)) {
+    'Failed to parse Cake version'
+    exit 1
+}
+if ([string]::IsNullOrEmpty($GitVersionVersion)) {
+    'Failed to parse GitVersion version'
+    exit 1
 }
 
 ###########################################################################
 # INSTALL .NET Core 3.x tools
 ###########################################################################
+
+$TOOLS_DIR = Join-Path $PSScriptRoot "tools"
+if ((Test-Path $PSScriptRoot) -and (-not (Test-Path $TOOLS_DIR))) {
+    Write-Verbose -Message "Creating tools directory..."
+    New-Item -Path $TOOLS_DIR -Type directory | out-null
+}
 
 # To see list of packageid, version and commands
 #      dotnet tool list --tool-path ./tools
@@ -71,13 +79,8 @@ Function Install-NetCoreTool {
         [string]$Version
     )
 
-    if (![string]::IsNullOrEmpty($Version)) {
-        $ToolPath = Join-Path $TOOLS_DIR '.store' | Join-Path -ChildPath $PackageId.ToLower() | Join-Path -ChildPath $Version
-        $ToolPathExists = Test-Path -Path $ToolPath -PathType Container
-    }
-    else {
-        $ToolPathExists = $false
-    }
+    $ToolPath = Join-Path $TOOLS_DIR '.store' | Join-Path -ChildPath $PackageId.ToLower() | Join-Path -ChildPath $Version
+    $ToolPathExists = Test-Path -Path $ToolPath -PathType Container
 
     $ExePath = (Get-ChildItem -Path $TOOLS_DIR -Filter "${ToolCommandName}*" -File | ForEach-Object FullName | Select-Object -First 1)
     $ExePathExists = (![string]::IsNullOrEmpty($ExePath)) -and (Test-Path $ExePath -PathType Leaf)
@@ -88,11 +91,10 @@ Function Install-NetCoreTool {
             & dotnet tool uninstall --tool-path $TOOLS_DIR GitReleaseManager.Tool
         }
 
-        if (![string]::IsNullOrEmpty($Version)) {
-            & dotnet tool install --tool-path $TOOLS_DIR --version $Version --configfile NuGet.public.config $PackageId
-            if ($LASTEXITCODE -ne 0) {
-                exit $LASTEXITCODE
-            }
+        & dotnet tool install --tool-path $TOOLS_DIR --version $Version --configfile NuGet.public.config $PackageId
+        if ($LASTEXITCODE -ne 0) {
+            "Failed to install $PackageId"
+            exit $LASTEXITCODE
         }
 
         $ExePath = (Get-ChildItem -Path $TOOLS_DIR -Filter "${ToolCommandName}*" -File | ForEach-Object FullName | Select-Object -First 1)
@@ -111,18 +113,13 @@ Install-NetCoreTool -PackageId 'GitVersion.Tool' -ToolCommandName 'dotnet-gitver
 # When using modules we have to add this
 & "$CakeExePath" ./build.cake --bootstrap
 
-if ($ShowVersion.IsPresent) {
-    & "$CakeExePath" --version
-}
-else {
-    # Build the argument list.
-    $Arguments = @{
-        target        = $Target;
-        configuration = $Configuration;
-        verbosity     = $Verbosity;
-    }.GetEnumerator() | ForEach-Object { "--{0}=`"{1}`"" -f $_.key, $_.value }
+# Build the argument list.
+$Arguments = @{
+    target        = $Target;
+    configuration = $Configuration;
+    verbosity     = $Verbosity;
+}.GetEnumerator() | ForEach-Object { "--{0}=`"{1}`"" -f $_.key, $_.value }
 
-    Write-Host "Running build script..."
-    & "$CakeExePath" ./build.cake $Arguments $ScriptArgs
-}
+Write-Host "Running build script..."
+& "$CakeExePath" ./build.cake $Arguments $ScriptArgs
 exit $LASTEXITCODE
--- build.sh
+++ build.sh
@@ -1,95 +1,60 @@
 #!/usr/bin/env bash
 
-##########################################################################
-# This is the Cake bootstrapper script for Linux and OS X.
-# This file was downloaded from https://github.com/cake-build/resources
-# Feel free to change this file to fit your needs.
-##########################################################################
-
-# Define directories.
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-TOOLS_DIR="$SCRIPT_DIR/tools"
-NUGET_EXE="$TOOLS_DIR/nuget.exe"
-CAKE_EXE="$TOOLS_DIR/Cake/Cake.exe"
-PACKAGES_CONFIG="$TOOLS_DIR/packages.config"
-PACKAGES_CONFIG_MD5="$TOOLS_DIR/packages.config.md5sum"
+###########################################################################
+# LOAD versions from build.config
+###########################################################################
 
-# Define md5sum or md5 depending on Linux/OSX
-MD5_EXE=
-if [[ "$(uname -s)" == "Darwin" ]]; then
-    MD5_EXE="md5 -r"
-else
-    MD5_EXE="md5sum"
+source $SCRIPT_DIR/build.config
+if [ "$CAKE_VERSION" = "" ]; then
+    echo "Failed to parse Cake version"
+    exit 1
+fi
+if [ "$GITVERSION_VERSION" = "" ]; then
+    echo "Failed to parse GitVersion version"
+    exit 1
 fi
 
-# Define default arguments.
-SCRIPT="build.cake"
-TARGET="Default"
-CONFIGURATION="Release"
-VERBOSITY="verbose"
-DRYRUN=
-SHOW_VERSION=false
-SCRIPT_ARGUMENTS=()
+###########################################################################
+# INSTALL .NET Core 3.x tools
+###########################################################################
 
-# Parse arguments.
-for i in "$@"; do
-    case $1 in
-        -s|--script) SCRIPT="$2"; shift ;;
-        -t|--target) TARGET="$2"; shift ;;
-        -c|--configuration) CONFIGURATION="$2"; shift ;;
-        -v|--verbosity) VERBOSITY="$2"; shift ;;
-        -d|--dryrun) DRYRUN="--dryrun" ;;
-        --version) SHOW_VERSION=true ;;
-        --) shift; SCRIPT_ARGUMENTS+=("$@"); break ;;
-        *) SCRIPT_ARGUMENTS+=("$1") ;;
-    esac
-    shift
-done
-
-# Make sure the tools folder exist.
+TOOLS_DIR="$SCRIPT_DIR/tools"
 if [ ! -d "$TOOLS_DIR" ]; then
   mkdir "$TOOLS_DIR"
 fi
 
-# Download NuGet if it does not exist.
-if [ ! -f "$NUGET_EXE" ]; then
-    echo "Downloading NuGet..."
-    curl -Lsfo "$NUGET_EXE" https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-    if [ $? -ne 0 ]; then
-        echo "An error occured while downloading nuget.exe."
-        exit 1
+function install_tool () {
+    packageId=$1
+    toolCommand=$2
+    version=$3
+
+    toolPath="$TOOLS_DIR/.store/${packageId}/$CAKE_VERSION"
+    exePath="$TOOLS_DIR/${toolCommand}"
+
+    if [ ! -d "$toolPathExists" || ! -d "$exePath" ]; then
+
+        if [ -d "$exePath" ]; then
+            dotnet tool uninstall --tool-path $TOOLS_DIR $packageId
+        fi
+
+        dotnet tool install --tool-path $TOOLS_DIR --version $version --configfile NuGet.public.config $packageId
+        if [ $? -ne 0 ]; then
+            echo "Failed to install ${packageId}"
+            exit 1
+        fi
     fi
-fi
 
-# Install/restore tools (i.e. Cake) using NuGet
-pushd "$TOOLS_DIR" >/dev/null
+    echo $exePath
+}
 
-# Check for changes in packages.config and remove installed tools if true.
-if [ ! -f $PACKAGES_CONFIG_MD5 ] || [ "$( cat $PACKAGES_CONFIG_MD5 | sed 's/\r$//' )" != "$( $MD5_EXE $PACKAGES_CONFIG | awk '{ print $1 }' )" ]; then
-    find . -type d ! -name . | xargs rm -rf
-fi
+# We use lower cased package ids, because toLower is not defined in bash
+CAKE_EXE=$(install_tool 'cake.tool' 'dotnet-cake' $CakeVersion)
+install_tool 'gitversion.tool' 'dotnet-gitversion' $GitVersionVersion > /dev/null 2>&1
 
-mono "$NUGET_EXE" install $PACKAGES_CONFIG -ExcludeVersion
-if [ $? -ne 0 ]; then
-    echo "Could not restore NuGet tools."
-    exit 1
-fi
+###########################################################################
+# RUN BUILD SCRIPT
+###########################################################################
 
-# save packages.config hash to disk
-$MD5_EXE "$PACKAGES_CONFIG" | awk '{ print $1 }' >| "$PACKAGES_CONFIG_MD5"
-
-popd >/dev/null
-
-# Make sure that Cake has been installed.
-if [ ! -f "$CAKE_EXE" ]; then
-    echo "Could not find Cake.exe at '$CAKE_EXE'."
-    exit 1
-fi
-
-# Start Cake
-if $SHOW_VERSION; then
-    exec mono "$CAKE_EXE" -version
-else
-    exec mono "$CAKE_EXE" $SCRIPT --verbosity=$VERBOSITY --configuration=$CONFIGURATION --target=$TARGET $DRYRUN "${SCRIPT_ARGUMENTS[@]}"
-fi
+(exec "$CAKE_EXE" build.cake --bootstrap) && (exec "$CAKE_EXE" build.cake "$@")
